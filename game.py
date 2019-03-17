f53003db7e3d8e38a134dd36fb831ccec971f9b1
err0x5dd <err0x5dd@tkleinert.de>
2019-03-17 22:01:16 +0100

    Fixed some error to hopefully prevent a crash

--- game.py
+++ game.py
@@ -29,6 +29,7 @@ class GameData:
 
         # check for running server
         servers = self.client.servers.get_all()
+	self.server = None
         for i in servers:
             if i.data_model.name == name:
                 self.server = i.data_model
@@ -38,7 +39,6 @@ class GameData:
     def start(self):
         if self.running == False:
             print("Starting " + self.name)
-            self.running = True
             response = self.client.servers.create(
                 self.name,
                 server_type=ServerType(name=self.servertype),
@@ -46,12 +46,13 @@ class GameData:
                 volumes=[Volume(self.volume)]
             )
             self.server = response.server;
-            while self.server.status != Server.STATUS_RUNNING:
-                print(self.server.status)
-                time.sleep(2)
-                serv = self.client.servers.get_by_id(self.server.id)
-                self.server = serv
-            print("Server is now running")
+            #while self.server.status != Server.STATUS_RUNNING:
+            #    print(self.server.status)
+            #    time.sleep(2)
+            #    serv = self.client.servers.get_by_id(self.server.id)
+            #    self.server = serv
+            #print("Server is now running")
+            self.running = True
         else:
             print(self.name + " is already running")
 
@@ -61,24 +62,29 @@ class GameData:
             self.client.servers.shutdown(self.server)
             while self.server.status != Server.STATUS_OFF:
                 print(self.server.status)
-                time.sleep(2)
+                time.sleep(5)
                 serv = self.client.servers.get_by_id(self.server.id)
                 self.server = serv
             print("Server is now stopped")
             self.client.volumes.detach(Volume(self.volume))
             self.client.servers.delete(self.server)
             self.running = False
+	    self.server = None
         else:
             print(self.name + " isn't running")
 
     def status(self):
         msg = self.name
-        if self.running:
-            msg += " is running under\n"
+        if self.server == None:
+	    msg += " isn't running"
+        else:
+            self.server = self.client.servers.get_by_id(self.server.id)
+            msg += " is in status "
+            msg += self.server.status
+            msg += "\n"
+            msg += "and has the IP:\n"
             ip = self.server.public_net.ipv4.ip
             msg += ip + "\n"
-        else:
-            msg += " isn't running"
         return msg
 
     def isRunning(self):
